11476695de4664c0c948b0a8e8c1d93d35768d18
IV20 <UIMIV20@gmail.com>
2018-04-07 15:51:05 +0100

    Initial commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RustAdminUI.Startup))]
+namespace RustAdminUI
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
